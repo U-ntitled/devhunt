b366498bb22c68d8d4e1b9d7478446a1c8a0ffb8
PyWendi <rakotondranaivogilbert21@gmail.com>
2023-03-05 02:59:52 +0300

    Setting home api route and updating some model

--- api/Models/UserPost.js
+++ api/Models/UserPost.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose")
 
 const UserPostSchema = mongoose.Schema({
-    owner_id: {type:String,require:true},
+    owner: [{type: mongoose.Schema.Types.ObjectId, ref: "user"}],
     description: {type:String,require:true},
     images: {type:Array,require:false},
     state: {type:String,require:true,default:false}, //SOLVED OR NOT SIMPLE POST
     topic: {type:String,require:true},
     challenge_state: {type:Boolean, default:false}, //Challenge resolved or not
     delay: {type:Date,require:false},
+    created_at : {type:Date, require:true, default:Date.now},
     num_comment: {type:Number,require:true,default:0},
     user_mentionned: {type:Array,require:false},
     type: {type:String, require:true}, //POST,SHARE,EVENT,CHALLLENGE
-    entity : {type:String, require:false,default:"user"}
+    comments : [{type: mongoose.Schema.Types.ObjectId, ref: "comment"}]
 },
 {
     timestamp : true
--- api/Routes/home.js
+++ api/Routes/home.js
@@ -1,27 +1,30 @@
 const route = require('express').Router(); 
+const coms = require("../Models/Comment")
 const User = require('../Models/Users')
 const uPost = require('../Models/UserPost');
+const Topic = require("../Models/Topics")
 
 
-// Envoie des Postes
+// SENDING POST
 route.get('/', async (req, res)=>{
     try{
+        // Getting all post first 
+        Topic.find({}).then((topics) => {
 
+            // Getting data
+            uPost.find({})
+            .sort({created_at : -1})
+            .limit(10)
+            .then((posts) => {
 
-        //Recuperation des postes utilisateurs par date de creation
-        uPost.find({created_at: -1})
-        .then((found)=>{
-            if(found){
-                res
-                .status(200)
-                .json(found)
-            }
-            else{
-                
-                res
-                .status(404)
-                .end()
-            }
+                data = {
+                    post : posts,
+                    topic : topics
+                }
+
+                res.status(200)
+                .json(data).end()
+            })
         })
     }
     catch(e){
@@ -31,6 +34,111 @@ route.get('/', async (req, res)=>{
     }
 })
 
+//SENDING SPECIFIC POST
+
+
+route.get('/post/:topic', async (req, res)=>{
+    try{
+        const topic = req.params.topic
+        uPost.find({topic: topic})
+        .limit(10)
+        .then((result)=>{
+            res
+            .status(200)
+            .json(result)
+        })
+    }
+    catch(e){
+        res
+        .status(404)
+        .end()
+    }
+
+})
+
+// Adding post
+route.post("/create-post", async (request, response) => {
+    const {owner,description,images,state,topic,challenge_state,delay,num_comment,type} = request.body
+    try {
+        const Upost = new uPost({
+            owner,
+            description,
+            images,
+            state,
+            topic,
+            challenge_state,
+            delay,
+            num_comment,
+            type
+        })
+
+        Upost.save()
+        response.cookie("post_id",Upost.id)
+        .status(200).end()
+
+    }catch(e) {
+        console.log(e)
+        response.status(400)
+        .json({data: "Error while creating form. Better check your connection."})
+    }
+    }
+})
+
+
+// Setting new comment in a post
+route.post("/create-comment",async (req, res) => {
+    const { post_id, text } = req.body
+    try{
+        const Comment = new coms({
+            post :req.cookies.post_id,
+            coms_user_id: req.cookies.user_id,
+            text: req.body.text, 
+        })
+    
+        Comment.save()
+    
+        uPost.findOne({topic : "topic"}).then((poste) => {
+            poste.comments.push(Comment.id)
+            poste.save()
+        })
+        res.status(200).end()
+    }
+    catch(e){
+        console.log(e)
+        response
+        .status(404)
+        .end({error: "Vous svez enter de mauvaises donnees"})
+    }
+})
+
+
+// Getting a specific post
+route.get("/show-post",(req,res) => {
+
+    // Getting Post Data 
+    try {
+        uPost
+        .findOne({id : req.body.post_id})
+        .then((post) => {
+    
+            // Extract user from the post data
+            User.findOne({id: post.owner[0]})
+            .select("fname lname profilePicture")
+            .then((user) => {
+                var data = {
+                    post : poste,
+                    user: user
+                }
+                res.status(200).json(data)
+            })
+    
+        })
+    }catch(e) {
+        console.log(e)
+        response.status(400)
+        .json({result:"Error while getting data, Please verify your connection"}).end()
+    }
+})
 
 
 module.exports = route
