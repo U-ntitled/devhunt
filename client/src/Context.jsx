70d30c02917d4ffe5fec451533a248b9e7d37768
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-03-04 12:51:15 +0300

    configuration du route front end

---
+++ client/src/Context.jsx
@@ -0,0 +1,23 @@
+import React,{createContext, useCallback, useEffect, useState} from 'react'
+import axios from 'axios'
+
+const MyContext = createContext({})
+const GlobalContext = ({children}) => {
+    const [connection,setConnection] = useState('')
+    const getCookie = useCallback( async() =>{
+      const cookie = await axios.get('http://localhost:8000/api/Routes/auth/')
+      setConnection(cookie)
+  },[])
+    useEffect(()=>{
+        getCookie()
+    },[getCookie])
+  return (
+    <MyContext.Provider value={{getCookie,connection}}>
+            {children}
+    </MyContext.Provider>
+      
+    
+  )
+}
+export default MyContext
+export  {GlobalContext}
