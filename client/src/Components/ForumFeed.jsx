429c1c78f77d506d865d149a8cdde6ca1c2c88cb
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-03-05 05:59:45 +0100

    Question page

--- client/src/Components/ForumFeed.jsx
+++ client/src/Components/ForumFeed.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import QuestionCard from './QuestionCard'
+import AskButton from './AskButton'
 
 function ForumFeed() {
-  const navigate = useNavigate()
+  
   return (
     <div className='w-[60%] px-5 py-4'>
            <div className='flex justify-end items-center'>
-               <button 
-                   type='button'
-                   className='bg-[#00CF35] text-[#f2f2f2] py-2 px-3 mb-4 '
-                   onClick={()=> navigate('ask')}
-                >
-                  Ask question
-                </button>
+                <AskButton/>
            </div>
            <div className='w-[1/2] h-[700px] overflow-y-auto flex flex-col '>
                  <QuestionCard/>
--- client/src/Components/Question.jsx
+++ client/src/Components/Question.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import backIcon from '../Image/Back.png'
+import AskButton from './AskButton'
 
 function Question() {
   return (
-    <div className='w-[60%] bg-black px-3 py-3'>
+    <div className='w-[60%] bg-black px-3 py-4'>
          <div className='flex justify-start items-center'>
-              <img src={backIcon} alt="back icon"/>
-              <span className='text-[#f2f2f2] ml-4'>Question</span>
+             <div>
+                 <img src={backIcon} alt="back icon"/>
+                <span className='text-[#f2f2f2] ml-6 text-xl'>Question</span>
+             </div>
+             {/* <button 
+                   type='button'
+                   className='bg-[#00CF35] text-[#f2f2f2] py-2 px-3 mb-4 '
+                   onClick={()=> navigate('ask')}
+                >
+                  Ask question
+             </button> */}
+             <AskButton/>
+             
+             
          </div>
         
           
