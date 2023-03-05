7c1740b99d409bb13dbeebf6ca215cff7142d8ae
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-03-05 01:25:36 +0100

    creation de la page forum

--- client/src/GeneraleRoute.jsx
+++ client/src/GeneraleRoute.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import AskedQuestion from './Components/AskedQuestion'
+import ForumFeed from './Components/ForumFeed'
+import Question from './Components/Question'
 import Confirmation from './Pages/Confirmation'
+import Forum from './Pages/Forum'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
-import Roote from './Pages/Roote'
+import Root from './Pages/Root'
 import Signup from './Pages/Signup'
 
 function GeneraleRoute() {
@@ -12,8 +16,13 @@ function GeneraleRoute() {
         <Route path='/login' element={<Login/>}/>       
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/confirmation' element={<Confirmation/>}/> 
-        <Route path='/' element={<Roote/>}>
+        <Route path='/' element={<Root/>}>
           <Route path='/home' element={<Home/>} index={true} />
+              <Route path='/forum' element={<Forum/>}>
+                    <Route path=''element={<ForumFeed/>} index={true}/>
+                    <Route path='question/:id' element={<Question/>}/>
+                    <Route path='ask' element={<AskedQuestion/>}/>
+              </Route>
         </Route>
     </Routes>
   )
